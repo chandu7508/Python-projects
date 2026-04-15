81b72a0dc212cb41a2a35b1545722ae2db37cc9e
chandu7508 <chandu7508@gmail.com>
2026-04-16 00:21:34 +0530

    Python Basics code addition

--- Practice coding/Python Bascis.py
+++ Practice coding/Python Bascis.py
@@ -1,13 +1,20 @@
-name = input('What is your name ? ').capitalize()
+"""name = input('What is your name ? ').capitalize()
 fav_color = input('What is your favorite color ? ').lower()
 name_last_letter_index = len(name)-1
 if name[name_last_letter_index] != 's':
     print(name + "'s favorite color is " + fav_color)
 else:
-    print(name + "' favorite color is " + fav_color)
+    print(name + "' favorite color is " + fav_color)"""
 
+# Positional Arguments
 
-"""
-Topics Learnt: variables, arithmetic & assignment operators, 
-strings(functions, indexing, manipulation), input() function
-"""
+def _function(c1,c2,c3):
+    print("a is: ",c1)
+    print("b is: ",c2)
+    print("c is: ",c3)
+
+a = 1
+b = 2
+c = 3
+
+_function(c2 = b,c1 = a,c3 = c)
